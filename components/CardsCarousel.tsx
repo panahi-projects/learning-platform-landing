4f3b54a8880b43a51c8c1f65056dcfe010b98839
SAEED.PANAHI <panahi.projects@gmail.com>
2025-01-13 15:23:07 +0330

    refactor CardCarousel component

--- components/CardsCarousel.tsx
+++ components/CardsCarousel.tsx
@@ -1,15 +1,8 @@
 "use client";
-import { FaFootballBall, FaPaintBrush } from "react-icons/fa";
-import { FaMusic } from "react-icons/fa6";
-import useEmblaCarousel from "embla-carousel-react";
-import React, { ReactNode, useCallback, useEffect, useState } from "react";
-import {
-  EmblaCarouselType,
-  EmblaEventType,
-  EmblaOptionsType,
-} from "embla-carousel";
 import "@/public/styles/embla.scss";
-import ProgramCard, { CardProps } from "./ProgramCard";
+import { EmblaOptionsType } from "embla-carousel";
+import useEmblaCarousel from "embla-carousel-react";
+import React, { ReactNode, useEffect, useState } from "react";
 import { DotButton, useDotButton } from "./DotButton";
 
 interface CardsCarouselProps {
@@ -23,7 +16,6 @@ interface CardsCarouselProps {
 const CardsCarousel: React.FC<CardsCarouselProps> = ({
   title,
   description,
-  options,
   children,
   onSlideChange,
 }) => {
@@ -31,55 +23,19 @@ const CardsCarousel: React.FC<CardsCarouselProps> = ({
   const { selectedIndex, scrollSnaps, onDotButtonClick } =
     useDotButton(emblaApi);
   const [centerindex, setCenterIndex] = useState<number | null>(null);
-  const [itemsInViewport, setItemsInViewport] = useState<number[]>([]);
-
-  const isConsecutive = (numbers: number[]): boolean => {
-    if (numbers.length <= 1) return true; // An empty or single-element array is considered consecutive
-
-    // Check if each number is exactly 1 greater than the previous number
-    for (let i = 1; i < numbers.length; i++) {
-      if (numbers[i] !== numbers[i - 1] + 1) {
-        return false;
-      }
-    }
-
-    return true;
-  };
-
-  const logSlidesInView = useCallback((emblaApi: EmblaCarouselType) => {
-    const slidesInViewport = emblaApi?.slidesInView();
-    setItemsInViewport(slidesInViewport || []);
-  }, []);
 
   useEffect(() => {
     if (emblaApi) {
-      emblaApi.on("slidesInView", logSlidesInView);
-
       // Listen for the slide change event and notify parent via callback
       emblaApi.on("select", () => {
         const newCenterIndex = emblaApi.selectedScrollSnap();
-        setCenterIndex(newCenterIndex); // Update centerIndex
+        setCenterIndex(newCenterIndex); // Update centerindex
         if (onSlideChange) {
           onSlideChange(newCenterIndex); // Call the custom event handler if provided
         }
       });
     }
-  }, [emblaApi, logSlidesInView, onSlideChange]);
-  useEffect(() => {
-    if (itemsInViewport) {
-      const isNormal = isConsecutive(itemsInViewport || []);
-
-      if (isNormal) {
-        setCenterIndex(itemsInViewport[1] || 0);
-      } else {
-        if (itemsInViewport[1] === 1) {
-          setCenterIndex(0);
-        } else {
-          setCenterIndex(itemsInViewport[1] + 1);
-        }
-      }
-    }
-  }, [itemsInViewport]);
+  }, [emblaApi, onSlideChange]);
 
   // Clone children and pass centerindex to each one
   const clonedChildren = React.Children.map(children, (child) => {
